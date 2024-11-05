d7037474233eae448c7187c6f4fb6da492708c02
Santosh Kumar Sah <Ssah5803@gmail.com>
2024-11-06 00:29:37 +0530

    url changed

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,51 +5,15 @@ import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import View from "@/components/View";
-//only for ppr
-// import { unstable_after as after } from "next/server";
 
-import hljs from "highlight.js"; // https://highlightjs.org
-
-// full options list (defaults)
 const md = markdownit({
-  // Enable HTML tags in source
   html: true,
-  // Use '/' to close single tags (<br />).
-  // This is only for full CommonMark compatibility.
   xhtmlOut: true,
-  // Convert '\n' in paragraphs into <br>
   breaks: true,
-  // CSS language prefix for fenced blocks. Can be
-  // useful for external highlighters.
   langPrefix: "language-",
-
-  // Autoconvert URL-like text to links
   linkify: true,
-
-  // Enable some language-neutral replacement + quotes beautification
-  // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.mjs
   typographer: true,
-
-  // Double + single quotes replacement pairs, when typographer enabled,
-  // and smartquotes on. Could be either a String or an Array.
-  //
-  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
   quotes: "“”‘’",
-
-  // Highlighter function. Should return escaped HTML,
-  // or '' if the source string is not changed and should be escaped externally.
-  // If result starts with <pre... internal wrapper is skipped.
-  // highlight: function (str, lang) {
-  //   console.log({ str, lang });
-  //   if (lang && hljs.getLanguage(lang)) {
-  //     try {
-  //       return hljs.highlight(str, { language: lang, ignoreIllegals: true })
-  //         .value;
-  //     } catch (e) {}
-  //   }
-  //   return "";
-  // },
 });
 
 // export const experimental_ppr = true; // which config same in next.config.ts
@@ -121,10 +85,8 @@ const Startup = async ({ params }: { params: Promise<{ id: string }> }) => {
           )}
         </div>
         <hr className="divider" />
-        {/* TODO: EDITOR SELECTED STARTUP */}
-        <Suspense fallback={<p className="view_skeleton">Loading...</p>}>
-          <View id={startupInfo?._id} />
-        </Suspense>
+
+        <View id={startupInfo?._id} />
       </section>
     </>
   );
